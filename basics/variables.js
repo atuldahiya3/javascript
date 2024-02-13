a155908745d08de69cfd184b77bf45ed22e2a4d5
atuldahiya3 <87331579+atuldahiya3@users.noreply.github.com>
2024-02-13 05:38:42 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 1121;
+let email= "atul@gmail.com"
+var money="1000"   //prefer not to use var because of its block scope and functional scope
+
+address= "sonipat";
+let occupation;
+
+// accountId=121111 //const variable can not be changed
+
+email= "dahiya@gmail.com"
+money="1200"
+address="MDU"
+
+console.table([accountId,email,money,address,occupation])
+
