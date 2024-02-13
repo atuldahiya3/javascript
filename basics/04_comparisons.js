4ee3cbec917e27de9385ed2f5db5aec388cd2404
atuldahiya3 <87331579+atuldahiya3@users.noreply.github.com>
2024-02-13 06:45:47 +0000

    comparisons

---
+++ basics/04_comparisons.js
@@ -0,0 +1,10 @@
+console.log("2">1) //js converts "2" to int by itself
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
