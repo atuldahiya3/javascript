8d7909d7d9cceabcd58d1496c9418254089a6cd6
atuldahiya3 <87331579+atuldahiya3@users.noreply.github.com>
2024-02-13 05:39:02 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //will treat all code as newer version
+
+let name="Atul"
+let age =21
+let isLoggedIn=True
+
+let state
+
+/*
+number=> a to power b
+bigint
+string=> ""
+boolean=> true/false
+null=> standalone value  , type of null is object
+undefined=>
+symbol=> unique
+*/
