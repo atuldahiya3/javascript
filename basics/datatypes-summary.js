520c36c30eb8c2d113888efa6ce37d159118d1df
atuldahiya3 <87331579+atuldahiya3@users.noreply.github.com>
2024-02-13 13:53:01 +0000

    datatypes-summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+// PRIMITIVE DATATYPES
+// 7 TYPES: NUMBER(INT, FLOAT), STRING,BOOL, BIGINT, NULL, UNDEFINED, SYMBOL
+
+// NON PRIMITIVE DATATYPES
+//ARRAY, OBJECT, FUNCTIONS
+
+//datatype of all non primitive datatypes is object
+
+const food_array=["apple","mango","banana"]
+
+let myObj={
+    name:"Atul",
+    age:"21"
+}
+
+const myFunction= function(){
+    console.log("this is inside function")
+}
